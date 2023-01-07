d7ac9cf364a8254911ca25e80e2da4b956601843
Jordan Rivera <101216666+Chiefer230@users.noreply.github.com>
2023-01-07 21:54:21 +0000

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,24 @@
+import socket
+import ipaddress
+import struct
+import requests
+
+
+class IP:
+    def __init__(self,buffer=None):
+        header = struct.unpack('<BBHHHBBH4s4s',buffer)
+        self.sourceIP = header[8]
+        self.destinationIP = header[9]
+        self.sourceAddress = ipaddress.ip_address(self.sourceIP)
+        self.destinationAddress = ipaddress.ip_address(self.destinationIP)
+
+host = '192.168.1.165'
+s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
+s.bind((host,0))
+while True:
+    ip_frame = IP(s.recvfrom(65535)[0][0:20])
+    print(f'{ip_frame.sourceAddress} -> {ip_frame.destinationAddress}')
+    source = requests.get(f'http://ip-api.com/json/{ip_frame.sourceAddress}')
+    locat = requests.get(f'http://ip-api.com/json/{ip_frame.destinationAddress}')
+    print(source.content)
+    print(locat.content)
